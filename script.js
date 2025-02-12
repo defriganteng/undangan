3088055ddd276f3dc6420950589ece93e0ea38cd
Bronze <defrianto.bronze@gmail.com>
2025-02-12 14:05:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ document.getElementById("openInvitation").addEventListener("click", function() {
     document.body.style.overflow = "auto"; // Mengaktifkan scroll setelah tombol ditekan
     document.getElementById("invitationContent").scrollIntoView({ behavior: "smooth" });
 });
+
+// Mengambil nama tamu dari URL
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+// Menampilkan nama tamu jika tersedia
+const guestName = getQueryParam("nama");
+if (guestName) {
+    document.getElementById("guestName").textContent = guestName;
+}
