f868d4abcd208399f8b9cea32a07ac2ec5eaadde
Bronze <defrianto.bronze@gmail.com>
2025-02-13 10:19:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ document.getElementById("openInvitation").addEventListener("click", function () 
     // Scroll ke isi undangan
     document.getElementById("content").scrollIntoView({ behavior: "smooth" });
 });
+
+
+// Fungsi untuk mengambil parameter dari URL
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+// Ambil nama dari parameter "nama"
+const guestName = getQueryParam("nama");
+
+// Periksa apakah ada nama di URL
+if (guestName) {
+    document.getElementById("guest-name").textContent = guestName;
+} else {
+    document.getElementById("guest-name").textContent = "Nama Tamu"; // Default jika tidak ada nama di URL
+}
