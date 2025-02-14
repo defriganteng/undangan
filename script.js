0241cad93c61c1fe1affa51db490862debb65e97
Bronze <defrianto.bronze@gmail.com>
2025-02-14 07:09:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("openInvitation").addEventListener("click", function () {
-    document.background.style.overflow = "auto"; // Mengaktifkan scroll
+    document.body.style.overflow = "auto"; // Mengaktifkan scroll
     let content = document.getElementById("content");
 
     // Memastikan musik latar diputar
